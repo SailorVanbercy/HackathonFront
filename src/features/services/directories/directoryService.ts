c27b7edb50cc706a73eaba0e195e8fb016a7af47
Sailor <la237887@student.helha.be>
2026-01-19 23:05:50 +0100

    implémentation de la récupération des dossier + ajout de dossier avec précision du dossier parent

---
+++ src/features/services/directories/directoryService.ts
@@ -0,0 +1,43 @@
+const API_URL = import.meta.env.VITE_API_URL + "/directories";
+
+export interface DirectoryDTO{
+    id:number;
+    name:string;
+    parentDirectoryId:number | 0;
+}
+export interface GetAllDirectoriesResponse{
+    directories:DirectoryDTO[];
+}
+export interface CreateDirectoryRequest{
+    name:string;
+    parentDirectoryId : number | null;
+}
+
+export const getAllDirectories = async (): Promise<DirectoryDTO[]> => {
+    const response = await fetch(API_URL, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "include"
+    });
+    if(!response.ok)throw new Error("Erreur lors de la récupération des dossiers");
+
+    const data : GetAllDirectoriesResponse = await response.json();
+    return data.directories;
+}
+
+export const createDirectory = async (req : CreateDirectoryRequest) : Promise<DirectoryDTO> => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body : JSON.stringify(req),
+        credentials: "include"
+    });
+
+    if(!response.ok) throw new Error("Erreur lors de la création du grimoire");
+
+    return response.json();
+};
