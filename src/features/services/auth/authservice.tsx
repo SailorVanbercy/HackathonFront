caf32cceef872a448e45475c6a6e0522385ff70b
Sailor <la237887@student.helha.be>
2026-01-19 14:35:23 +0100

    registerPage fonctionnelle

--- src/features/services/auth/authservice.tsx
+++ src/features/services/auth/authservice.tsx
@@ -1,26 +1,50 @@
-import type {LoginRequest, RegisterRequest} from "../../../shared/DTO/users/users.ts";
+import type {
+    LoginRequest,
+    RegisterRequest,
+    RegisterUserResponse,
+    UserResponse
+} from "../../../shared/DTO/users/users.ts";
 
-const API_URL =import.meta.env.VITE_API_URL + "/users";
-export const login : (user : LoginRequest) => Promise<void> = async (user : LoginRequest) => {
-    const response =  await fetch(API_URL + "/login", {
-        method:"POST",
-        headers : {
-            "Content-Type" :"application/json"
+const API_URL =import.meta.env.VITE_API_URL + "/auth";
+export const login = async (user : LoginRequest) : Promise<void> => {
+    const response =await fetch(API_URL + "/login", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
         },
-        body : JSON.stringify(user)
+        body: JSON.stringify(user),
+        credentials: "include"
     });
-
     if(!response.ok){
-
+        throw new Error("Erreur de connexion");
     }
 }
 
-export const register : (user : RegisterRequest) => Promise<void> = async (user : RegisterRequest) => {
-    return await fetch(API_URL + "/register", {
-        method:"POST",
-        headers : {
-            "Content-Type" :"application/json"
+export const register = async (user : RegisterRequest): Promise<RegisterUserResponse> => {
+    const response = await fetch(API_URL + "/register", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
         },
-        body : JSON.stringify(user)
+        body: JSON.stringify(user),
+        credentials: "include"
     });
+    if(!response.ok){
+        throw new Error("Erreur lors de la création du compte");
+    }
+    return response.json();
+}
+
+export const getCurrentUser = async () : Promise<UserResponse> => {
+    const response = await fetch(API_URL + "/me", {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        credentials: "include"
+    });
+    if(!response.ok){
+        throw new Error("Erreur lors de la récupération des données utilisateur");
+    }
+    return response.json();
 }
--- src/shared/DTO/users/users.ts
+++ src/shared/DTO/users/users.ts
@@ -3,8 +3,20 @@ export interface LoginRequest{
     password:string;
 }
 export interface RegisterRequest{
+    name:string;
+    firstName:string;
+    email:string;
+    password:string;
+}
+
+export interface UserResponse{
+    id : number;
     firstName:string;
     lastName:string;
     email:string;
-    password:string;
+    createdAt : Date;
+}
+export interface RegisterUserResponse{
+    id:number;
+    email:string;
 }
