02d1fb3ae249b054a57b7bc8f003640ca2138f23
Sailor <la237887@student.helha.be>
2026-01-19 15:01:55 +0100

    Login page et /me fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
 
 import './App.css';
-import HomePage from './features/layout/HomePage';
-import Footer from './features/components/Footer';
+// import HomePage from './features/layout/HomePage';
+// import Footer from './features/components/Footer';
+import LoginPage from "./features/layout/login/LoginPage.tsx";
+import {Route, Routes} from "react-router";
+import RegisterPage from "./features/layout/register/RegisterPage.tsx";
+import HomePage from "./features/layout/HomePage.tsx";
 
 function App() {
     return (
         <div className="app">
-            <main className="app-main">
-                <HomePage user={""} onCreateFolder={function(): void {
-                    throw new Error("Function not implemented.");
-                } } onOpenRecent={function(): void {
-                    throw new Error("Function not implemented.");
-                } } />
-            </main>
-            <Footer />
+          <Routes>
+            <Route
+            path={"/login"}
+            element={<LoginPage/>}>
+            </Route><Route
+            path={"/register"}
+            element={<RegisterPage/>}>
+            </Route>
+            <Route
+            path={"/home"}
+            element={<HomePage user={""} onCreateFolder={function(): void {
+                throw new Error("Function not implemented.");
+            } } onOpenRecent={function(): void {
+                throw new Error("Function not implemented.");
+            } }/>}>
+            </Route>
+            </Routes>
         </div>
+        // <div className="app">
+        //     <main className="app-main">
+        //         <HomePage user={""} onCreateFolder={function(): void {
+        //             throw new Error("Function not implemented.");
+        //         } } onOpenRecent={function(): void {
+        //             throw new Error("Function not implemented.");
+        //         } } />
+        //     </main>
+        //     <Footer />
+        // </div>
     );
 }
 
